8acea6f4a7784d14f055674eb1f074c3def7eb5a
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-07-21 13:27:17 +0545

    Add EVENT and TYPE tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,21 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+// Socket IO Events
+var EVENT = {
+  CONNECTION: 'connection',
+  DISCONNECT: 'disconnect',
+  MESSAGE: 'message'
+};
+
+// Custom Event Types
+var TYPE = {
+  MESSAGE: 'MESSAGE',
+  CONNECTION: 'CONNECTION',
+  DISCONNECTION: 'DISCONNECTION',
+  NEW_CONNECTION: 'NEW_CONNECTION'
+};
+
 var socketHelper = require('../lib/socket-helper');
 
 describe('Socket Helper Imports', function() {
@@ -21,12 +36,17 @@ describe('Socket Helper Imports', function() {
       var eventType = 'SAMPLE_EVENT';
       var eventPayload = { data: 'Hello World' };
 
-      var event = socketHelper.createEvent(eventType, eventPayload, 'prop1', 'prop2');
+      var event = socketHelper.createEvent(
+        eventType,
+        eventPayload,
+        'prop1',
+        'prop2'
+      );
 
       expect(event).to.have.all.keys('type', 'payload');
 
-      expect(event.type).to.equal(eventType);
-      expect(event.payload).to.equal(eventPayload);
+      expect(event.type).to.deep.equal(eventType);
+      expect(event.payload).to.deep.equal(eventPayload);
     });
 
     it('should return an FSA compliant object when payload is not provided', function() {
@@ -36,8 +56,8 @@ describe('Socket Helper Imports', function() {
 
       expect(event).to.have.all.keys('type', 'payload');
 
-      expect(event.type).to.equal(eventType);
-      expect(event.payload).to.equal(undefined);
+      expect(event.type).to.deep.equal(eventType);
+      expect(event.payload).to.deep.equal(undefined);
     });
 
     it('should return an FSA compliant object when no properties are provided', function() {
@@ -45,8 +65,18 @@ describe('Socket Helper Imports', function() {
 
       expect(event).to.have.all.keys('type', 'payload');
 
-      expect(event.type).to.equal(undefined);
-      expect(event.payload).to.equal(undefined);
+      expect(event.type).to.deep.equal(undefined);
+      expect(event.payload).to.deep.equal(undefined);
+    });
+  });
+
+  describe('EVENT and TYPE', function() {
+    it('should return correct EVENT', function() {
+      expect(socketHelper.EVENT).to.deep.equal(EVENT);
+    });
+
+    it('should return correct TYPE', function() {
+      expect(socketHelper.TYPE).to.deep.equal(TYPE);
     });
   });
 });
